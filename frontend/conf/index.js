9c1c6b246b54374791d0cbf9d860950f5e169244
CrioUser <crio-user@criodo.com>
2022-03-09 15:05:37 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURE_DETAILS

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,5 +1,5 @@
 
-const config = { backendEndpoint: "http://3.111.210.203:8082" };
+const config = { backendEndpoint: "http://3.111.179.172:8082" };
 
 
 export default config;
--- frontend/modules/adventure_details_page.js
+++ frontend/modules/adventure_details_page.js
@@ -4,33 +4,83 @@ import config from "../conf/index.js";
 function getAdventureIdFromURL(search) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Get the Adventure Id from the URL
-
-
-  // Place holder for functionality to work in the Stubs
-  return null;
+  const params = new URLSearchParams(search);
+  return params.get("adventure");
 }
 //Implementation of fetch call with a paramterized input based on adventure ID
 async function fetchAdventureDetails(adventureId) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Fetch the details of the adventure by making an API call
-
-
-  // Place holder for functionality to work in the Stubs
-  return null;
+  try{
+    let url=config.backendEndpoint+('/adventures/detail/?adventure='+adventureId);
+    let response=await fetch(url);
+    let data=await response.json();
+    return data;
+  }
+    catch(err){
+      return null;
+    }
 }
 
 //Implementation of DOM manipulation to add adventure details to DOM
 function addAdventureDetailsToDOM(adventure) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the details of the adventure to the HTML DOM
-
+try{
+let parentname=document.getElementById('adventure-name');
+let parentsubtitle=document.getElementById('adventure-subtitle');
+let parentphoto=document.getElementById('photo-gallery');
+let parentcontent=document.getElementById('adventure-content');
+parentname.textContent=adventure.name;
+parentsubtitle.textContent=adventure.subtitle;
+for(let i=0;i<adventure.images.length;i++){
+  let creatediv=document.createElement('div');
+  creatediv.innerHTML=`<img class="activity-card-image"src="${adventure.images[i]}" />`;
+ parentphoto.append(creatediv);
+}
+parentcontent.textContent=adventure.content;
+}
+catch(e){
+  return Error(e);
+}
 }
 
 //Implementation of bootstrap gallery component
 function addBootstrapPhotoGallery(images) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the bootstrap carousel to show the Adventure images
-
+  try{
+    let parentphoto=document.getElementById('photo-gallery');
+    parentphoto.innerHTML=`<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">`;
+    let creatediv=document.createElement('div');
+    creatediv.className="carousel-inner";
+    for(let i=0;i<images.length;i++){
+      let imgdiv=document.createElement('div');
+      if(i==0){
+        imgdiv.className="carousel-item active";
+      }
+     else{
+       imgdiv.className="carousel-item";
+     }
+     imgdiv.innerHTML=`<img class="d-block w-100" src="${images[i]}">`
+     creatediv.append(imgdiv);
+    }
+    console.log(creatediv);
+    let idelem=document.getElementById('carouselExampleControls');
+    idelem.appendChild(creatediv);
+    idelem.innerHTML+=`<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+    `;
+    console.log(parentphoto);
+    }
+  catch(e){
+    return Error(e);
+  }
 }
 
 //Implementation of conditional rendering of DOM based on availability
