965c93fb1693129da5e2c50bbad30ac260d61723
mnjoshi291 <31077340+mnjoshi291@users.noreply.github.com>
2022-12-24 16:57:33 +0530

    Update reservation_page.js

--- frontend/modules/reservation_page.js
+++ frontend/modules/reservation_page.js
@@ -4,13 +4,15 @@ import config from "../conf/index.js";
 async function fetchReservations() {
   // TODO: MODULE_RESERVATIONS
   // 1. Fetch Reservations by invoking the REST API and return them
-try{
-let response=await fetch(config.backendEndpoint+'/reservations');
-let data=await response.json();
-return data;
-}catch(err){
-  return null;
-}
+  try{
+    let x = await fetch(`${config.backendEndpoint}/reservations/`);
+    let y = await x.json();
+    return y;
+  } catch(err){
+      console.log(err);
+      return null;
+  }
+// Place holder for functionality to work in the Stubs
 }
 
 //Function to add reservations to the table. Also; in case of no reservations, display the no-reservation-banner, else hide it.
@@ -19,51 +21,52 @@ function addReservationToTable(reservations) {
   // 1. Add the Reservations to the HTML DOM so that they show up in the table
 
   //Conditionally render the no-reservation-banner and reservation-table-parent
-
+     if(reservations.length === 0){
+      document.getElementById("no-reservation-banner").style.display = "block";
+      document.getElementById("reservation-table-parent").style.display = "none";
+      }else {
+        document.getElementById("reservation-table-parent").style.display = "block";
+        document.getElementById("no-reservation-banner").style.display = "none";
+    } 
   /*
     Iterating over reservations, adding it to table (into div with class "reservation-table") and link it correctly to respective adventure
     The last column of the table should have a "Visit Adventure" button with id=<reservation-id>, class=reservation-visit-button and should link to respective adventure page
-
+  
     Note:
     1. The date of adventure booking should appear in the format D/MM/YYYY (en-IN format) Example:  4/11/2020 denotes 4th November, 2020
     2. The booking time should appear in a format like 4 November 2020, 9:32:31 pm
   */
- let ele=document.getElementById('no-reservation-banner');
- let ele1=document.getElementById('reservation-table-parent');
- if(reservations.length==0){
-   ele.style.display="block";
- }
- else{
-  ele.style.display="none";
- }
-if(reservations.length>0){
-ele1.style.display="block";
-let parentele=document.getElementById('reservation-table');
-//let url=window.location.href;
+    console.log(reservations);
+    let table = document.getElementById("reservation-table");
+    reservations.forEach(element => {
+        
+        let tableRow = document.createElement("tr");
+        tableRow.innerHTML = 
+         ` <td>${element.id}</td>
+           <td>${element.name}</td>
+           <td>${element.adventureName}</td>
+           <td>${element.person}</td>
+           <td>${new Date(element.date).toLocaleDateString("en-IN")}</td>
+           <td>${element.price}</td>
+           <td>${new Date(element.time).toLocaleString("en-IN",{
+             year:"numeric",
+             day: "numeric",
+             month:"long",
+             hour: "numeric",
+             minute: "numeric",
+             second:"numeric",
+             hour12 :true,
+           })}</td>
+           <td><div class="reservation-visit-button" id=${element.id}>
+              <a href=/frontend/pages/adventures/detail/?adventure=${element.adventure}>
+              Visit Adventure
+              </a>
+              </div>
+              </td>
+          `;
 
-for(let i=0;i<reservations.length;i++){
-  //let finalurl=url.replace('/reservations','/detail/?adventure='+reservations[i].adventure)
-  var date = new Date(reservations[i].date);
-  var time=new Date(reservations[i].time);
-  const options = {day: 'numeric',month: 'long',year: 'numeric',   };
-  let createtable= document.createElement("tr");
-  createtable.innerHTML = `
-      <td>${reservations[i].id}</td>
-      <td>${reservations[i].name}</td>
-      <td>${reservations[i].adventureName}</td>
-      <td>${reservations[i].person}</td>
-      <td>${date.toLocaleDateString('en-IN')}</td>
-      <td>${reservations[i].price}</td>
-      <td>${time.toLocaleDateString('en-IN',options)+', '+time.toLocaleTimeString('en-IN')}</td>
-      <td id=${reservations[i].id}><a href = /frontend/pages/adventures/detail/?adventure=${reservations[i].adventure}><button class='reservation-visit-button'>Visit Adventures</button></a></td>
-      `;
-      parentele.append(createtable);
-}
-console.log(parentele);
-}
-else{
-  ele1.style.display="none";
-}
+      table.appendChild(tableRow);
+    }); 
 }
 
 export { fetchReservations, addReservationToTable };
